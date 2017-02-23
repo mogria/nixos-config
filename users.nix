c3a34af48f96855c2052864e471eacd812aa6e8f
Mogria <m0gr14@gmail.com>
2017-02-23 15:45:47 +0100

    add user account for loraine

--- users.nix
+++ users.nix
@@ -5,13 +5,21 @@
   users.enforceIdUniqueness = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.mogria = {
-    isNormalUser = true;
-    uid = 1000;
-    extraGroups = ["wheel" "mogria"];
-    shell = pkgs.zsh;
+  users.extraUsers = {
+    mogria = {
+      isNormalUser = true;
+      uid = 1000;
+      extraGroups = ["wheel" "mogria"];
+      shell = pkgs.zsh;
+    };
+
+    loraine = {
+      isNormalUser = true;
+      uid = 1001;
+      extraGroups = ["loraine"];
+    };
   };
 
-  users.groups = [{ gid = 1000; name = "mogria"; }];
+  users.groups = [{ gid = 1000; name = "mogria"; } { gid = 1001; name = "loraine"; }];
 
 }
