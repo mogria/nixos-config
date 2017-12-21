bab6afd88242ee067762fd9871eb48a7c07d0e36
Mogria <m0gr14@gmail.com>
2017-12-21 21:29:37 +0100

    cleanup system packages, remove nix-home, because home-manager is now in use

--- system-packages.nix
+++ system-packages.nix
@@ -3,13 +3,8 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    wget
-    (import ./vim.nix)
-    ((pkgs.callPackage ./nix-home/package.nix) { })
-    git
-    chromium
-    direnv
     wireshark-gtk
+    vlc
     filezilla
     keepass
     thunderbird
@@ -19,11 +14,17 @@
     psmisc
     libreoffice
     evince
+
+    # shell / development stuff
+    wget
+    direnv
+    git
     zsh
     zsh-completions
     zsh-navigation-tools
     nix-zsh-completions
     oh-my-zsh
+    (import ./vim.nix)
     # discord
   ];
 }
