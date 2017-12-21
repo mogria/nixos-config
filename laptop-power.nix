095b2cf2dd95b46d8046c2c2d905058b99b1f2fa
Mogria <m0gr14@gmail.com>
2017-12-21 21:25:58 +0100

    new power settings for new laptop

--- laptop-power.nix
+++ laptop-power.nix
@@ -5,35 +5,34 @@
 
   powerManagement.enable = true;
   powerManagement.scsiLinkPolicy = "min_power";
-  powerManagement.cpuFreqGovernor = "ondemand";
+  powerManagement.cpuFreqGovernor = "powersave";
 
   powerManagement.powerUpCommands = ''
-    echo '1' > '/sys/module/snd_hda_intel/parameters/power_save'
-    echo '1500' > '/proc/sys/vm/dirty_writeback_centisecs'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-2/device/power/control'
-    echo 'auto' > '/sys/bus/usb/devices/2-1.6/power/control'
-    echo 'auto' > '/sys/bus/usb/devices/4-1.6/power/control'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-0/device/power/control'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-1/device/power/control'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-3/device/power/control'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-4/device/power/control'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-5/device/power/control'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-6/device/power/control'
-    echo 'auto' > '/sys/bus/i2c/devices/i2c-7/device/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.2/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1c.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:02.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:04.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:14.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:16.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1a.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1b.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1d.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.3/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.6/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:02:00.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:00.0/power/control'
-    echo 'auto' > '/sys/bus/pci/devices/0000:00:1c.1/power/control'
+    echo 'min_power' > '/sys/class/scsi_host/host0/link_power_management_policy';
+    echo '1500' > '/proc/sys/vm/dirty_writeback_centisecs';
+    echo '1' > '/sys/module/snd_hda_intel/parameters/power_save';
+    echo 'min_power' > '/sys/class/scsi_host/host1/link_power_management_policy';
+    echo 'min_power' > '/sys/class/scsi_host/host2/link_power_management_policy';
+    echo 'auto' > '/sys/bus/usb/devices/1-4/power/control';
+    echo 'auto' > '/sys/bus/i2c/devices/i2c-3/device/power/control';
+    echo 'auto' > '/sys/bus/usb/devices/1-1/power/control';
+    echo 'auto' > '/sys/bus/usb/devices/1-5/power/control';
+    echo 'auto' > '/sys/bus/i2c/devices/i2c-1/device/power/control';
+    echo 'auto' > '/sys/bus/i2c/devices/i2c-2/device/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:14.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:00.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.3/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:02.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.4/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:17.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:03:00.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:16.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:01:00.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:04:00.1/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:14.2/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:04:00.0/power/control';
+    echo 'auto' > '/sys/bus/pci/devices/0000:00:1f.2/power/control';
+    ethtool -s enp4s0f1 wol d;
   '';
 }
