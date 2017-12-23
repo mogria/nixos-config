cdad54c351cb2bb285c95b02435f1ade2b3bad11
Mogria <m0gr14@gmail.com>
2017-12-23 19:53:52 +0100

    use ctrlp instead of NERDTree

--- vim.nix
+++ vim.nix
@@ -13,22 +13,6 @@ vim_configurable.customize {
     set number
     set hlsearch
 
-    " open and close nerd tree automatically
-    autocmd VimEnter * NERDTree
-    function! s:CloseIfOnlyControlWinLeft()
-      if winnr("$") != 1
-        return
-      endif
-      if (exists("t:NERDTreeBufName") && bufwinnr(t:NERDTreeBufName) != -1)
-            \ || &buftype == 'quickfix'
-        q
-      endif
-    endfunction
-    augroup CloseIfOnlyControlWinLeft
-      au!
-      au BufEnter * call s:CloseIfOnlyControlWinLeft()
-    augroup END
-
     " Start interactive EasyAlign in visual mode (e.g. vipga)
     xmap ga <Plug>(EasyAlign)
 
@@ -43,7 +27,7 @@ vim_configurable.customize {
   vimrcConfig.vam.knownPlugins = pkgs.vimPlugins;
   vimrcConfig.vam.pluginDictionaries = [
     { names = [
-        "The_NERD_tree"
+        "ctrlp"
         "vundle"
         "vim-addon-php-manual"
         "vim-easy-align"
