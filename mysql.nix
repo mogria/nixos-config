933bf1548d15c310e2a05875928a7b17cef29918
Mogria <m0gr14@gmail.com>
2018-01-04 22:22:32 +0100

    lock down mysql instance

--- mysql.nix
+++ mysql.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  services.mysql.enable = true;
-  services.mysql.package = pkgs.mariadb;
+  services.mysql = {
+    enable = true;
+    bind = "127.0.0.1";
+    package = pkgs.mariadb;
+    dataDir = "/home/mysql/datadir";
+  };
 }
