8329be14ff04f41bd97a838952b41d8ab39e6d26
Mogria <m0gr14@gmail.com>
2018-10-17 23:46:11 +0200

    add current graphical.nix configuration as a draft

--- graphical.nix
+++ graphical.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Enable the X11 windowing system.
@@ -6,13 +6,67 @@
   services.xserver.layout = "us";
   services.xserver.xkbModel = "pc104";
   services.xserver.xkbOptions = "terminate:ctrl_alt_bksp, eurosign:e";
-  # services.xserver.videoDrivers = [ "nvidia" ]; # 303 #173
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.slim.enable = true;
+  services.xserver.displayManager.slim = {
+    enable = true;
+    # this is how you would specify your slim theme, but nixos has changed
+    # to lightdm anyway soo....
+    # theme = pkgs.writeTextFile {
+    # name = "slim-theme";
+    #  text = ''
+    #  '';
+    # };
+  };
   services.xserver.desktopManager.xfce = {
     enable = true;
+    thunarPlugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-dropbox-plugin
+    ];
   };
+
+  services.xserver.updateDbusEnvironment = true;
+
+  programs.dconf.enable = true; # required for settings in gnome-terminal
+  services.gnome3 = {
+    # this is required for mounting android phones
+    # over mtp://
+    gvfs.enable = true;
+    gnome-terminal-server.enable = true;
+  };
+
+  # services.udev.packages = [ pkgs.gnome3.gnome_settings_daemon ];
+
+  environment.systemPackages = [
+    # pkgs.gnome3.gconf, removed use gnome3.gconf instead
+    pkgs.gnome3.gnome_terminal
+
+    # XFCE plugins
+    pkgs.xfce.xfce4-systemload-plugin
+    pkgs.xfce.xfce4-weather-plugin
+    pkgs.xfce.xfce4-whiskermenu-plugin
+    # pkgs.xfce.xfce4-timer-plugin
+    pkgs.xfce.xfce4-clipman-plugin
+  ];
+
+  /* may not be required anymore, testing...
+   environment.variables.GIO_EXTRA_MODULES = [ "${lib.getLib pkgs.gnome3.dconf}/lib/gio/modules"
+                                             "${pkgs.gnome3.glib_networking.out}/lib/gio/modules"
+                                             "${pkgs.gnome3.gvfs}/lib/gio/modules" ]; */
+
+  services.dbus.packages = [
+    # never printed but this might be useful
+    (lib.mkIf config.services.printing.enable pkgs.system-config-printer)
+
+    # this is required or else a warning appears on XFCE startup
+    # that no service for blueman has been found,...
+    (lib.mkIf config.hardware.bluetooth.enable pkgs.blueman)
+  ];
+
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.desktopManager.gnome3 = {
+  #   enable = true;
+  # };
 
   # environment.gnome3.excludePackages = [
   #   pkgs.gnome3.evolution
