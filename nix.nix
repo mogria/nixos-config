5572f977b74cbd2f1314417a21b553373e5709f1
Mogria <m0gr14@gmail.com>
2018-12-29 19:49:05 +0100

    more build jobs at the same time, autooptimise the store

--- nix.nix
+++ nix.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  nix.useSandbox = true;
+  nix = {
+    useSandbox = true; #might not be required anymore in the future
+    buildCores = 16; #might not be required anymore in the future
+    maxJobs = 16; #number of cores
+    autoOptimiseStore = true;
+  };
 
   nixpkgs.overlays = [
     ( import ./overlays/nvidia.nix )
