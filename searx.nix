1cd10322fd84676021ed2b213a567ff6f3db42e9
Mogria <m0gr14@gmail.com>
2018-12-29 19:45:09 +0100

    enable local anonymous searx instance, to search multiple search engines through tor

--- searx.nix
+++ searx.nix
@@ -1,7 +1,27 @@
 { config, pkgs, lib, ... }:
 
 {
+
+  networking.extraHosts = ''
+    127.0.0.1 searx.local
+    ::11 searx.local
+  '';
+
   services.searx = {
     enable = true;
+    configFile = ./searx-settings.yml;
+  };
+
+  # client is a fast socks port which only
+  # creates circuits every 10 minutes,
+  # especially suitable for HTTP
+  services.tor = {
+    enable = true;
+    client = {
+      enable = true;
+      # these are the defaults, but make sure listen address is localhost
+      socksListenAddress = "127.0.0.1:9050"; 
+      socksListenAddressFaster = "127.0.0.1:9063"; #this is the faster port
+    };
   };
 }
