d16a771d5e5408be2fb635330b09ad224dd79ecf
Mogria <m0gr14@gmail.com>
2018-12-29 19:48:21 +0100

    enable kdeconnect

--- graphical.nix
+++ graphical.nix
@@ -49,6 +49,32 @@
     pkgs.xfce.xfce4-whiskermenu-plugin
     # pkgs.xfce.xfce4-timer-plugin
     pkgs.xfce.xfce4-clipman-plugin
+    pkgs.xfce.xfce4-taskmanager
+
+    # Required to run to load kdeconnect: 
+    #   qdbus org.kde.kded /kded loadModule kdeconnect
+    # on startup
+    pkgs.qt5.qttools
+    pkgs.kdeconnect
+    pkgs.kde-cli-tools
+  ];
+
+  # Open ports for KDE Connect
+  networking.firewall.allowedTCPPorts = [
+                        1714 1715 1716 1717 1718 1719
+    1720 1721 1722 1723 1724 1725 1726 1727 1728 1729
+    1730 1731 1732 1733 1734 1735 1736 1737 1738 1739
+    1740 1741 1742 1743 1744 1745 1746 1747 1748 1749
+    1750 1751 1752 1753 1754 1755 1756 1757 1758 1759
+    1760 1761 1762 1763 1764
+  ];
+  networking.firewall.allowedUDPPorts = [
+                        1714 1715 1716 1717 1718 1719
+    1720 1721 1722 1723 1724 1725 1726 1727 1728 1729
+    1730 1731 1732 1733 1734 1735 1736 1737 1738 1739
+    1740 1741 1742 1743 1744 1745 1746 1747 1748 1749
+    1750 1751 1752 1753 1754 1755 1756 1757 1758 1759
+    1760 1761 1762 1763 1764
   ];
 
   /* may not be required anymore, testing...
@@ -63,6 +89,9 @@
     # this is required or else a warning appears on XFCE startup
     # that no service for blueman has been found,...
     (lib.mkIf config.hardware.bluetooth.enable pkgs.blueman)
+
+    #   required to load kdeconnect, for kdeinit5
+    pkgs.kdeFrameworks.kdbusaddons
   ];
 
   # Enable the GNOME Desktop Environment.
