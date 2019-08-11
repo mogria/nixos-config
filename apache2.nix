56ce7c190fb40e89f362fa054b14975c3061a9ad
Mogria <m0gr14@gmail.com>
2019-08-11 06:41:27 +0200

    update development apache config

--- apache2.nix
+++ apache2.nix
@@ -3,14 +3,20 @@
 {
   networking.extraHosts = ''
     127.0.0.1 esther-loeffel.dev.local
-    127.0.0.1 esther-loeffel-before-upgrade.dev.local
+    127.0.0.1 baukombinat.dev.local
+    # 127.0.0.1 esther-loeffel-before-upgrade.dev.local
     ::1  esther-loeffel.dev.local
-    ::1  esther-loeffel-before-upgrade.dev.local
+    ::1  baukombinat.dev.local
+    #::1  esther-loeffel-before-upgrade.dev.local
   '';
 
   services.httpd = {
     enable = true;
     enablePHP = true;
+    phpOptions = ''
+        post_max_size = 20M
+        upload_max_filesize = 20M
+    '';
     # phpPackage = pkgs.php56; php 5.6 is not available anymore in nixpkgs
     adminAddr = "root@localhost";
     documentRoot = "/var/www";
@@ -24,15 +30,32 @@
           DirectoryIndex index.php
         '';
       }
+      # {
+        # listen = [ { ip = "127.0.0.1"; port = 80; } ];
+        # hostName = "esther-loeffel-before-upgrade.dev.local";
+        # documentRoot = "/var/www/esther-loeffel-before-upgrade";
+        # extraConfig = ''
+          # DirectoryIndex index.php
+        # '';
+      # }
       {
         listen = [ { ip = "127.0.0.1"; port = 80; } ];
-        hostName = "esther-loeffel-before-upgrade.dev.local";
-        documentRoot = "/var/www/esther-loeffel-before-upgrade";
+        hostName = "baukombinat.dev.local";
+        documentRoot = "/var/www/baukombinat";
         extraConfig = ''
           DirectoryIndex index.php
         '';
       }
     ];
+
+    extraConfig = ''
+      <Directory /var/www>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Order allow,deny
+        allow from all
+      </Directory>
+    '';
 
   };
 }
