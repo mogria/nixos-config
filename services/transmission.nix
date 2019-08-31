4945b6affb71dfd0e44401da263f4a320fd9345b
Mogria <m0gr14@gmail.com>
2019-08-31 17:38:51 +0200

    transmission: add update

--- services/transmission.nix
+++ services/transmission.nix
@@ -11,12 +11,20 @@
       "incomplete-dir" = "/srv/storage/Transmission/Incomplete-Downloads";
       "incomplete-dir-enabled" = true;
 
+      /* web interface, accessible from local network */
       "rpc-enabled" = true;
       "rpc-bind-address" = "0.0.0.0";
       "rpc-whitelist" = "127.0.0.1,192.168.*.*";
       "rpc-host-whitelist" = "void,192.168.*.*";
       "rpc-host-whitelist-enabled" = true;
-      "lpd-enabled" = true;
+
+      "lpd-enabled" = true; /* local peer discovery */
+
+      /* ip blocklist */
+      "blocklist-enabled" = true;
+      "blocklist-updates-enabled" = true;
+      "blocklist-url" = "http://john.bitsurge.net/public/biglist.p2p.gz";
+      /* taken from here: https://giuliomac.wordpress.com/2014/02/19/best-blocklist-for-transmission/ */
     };
   };
 
