86d1ec177c2eac9d6f0b00a4472c3f19d8861037
Mogria <m0gr14@gmail.com>
2019-08-31 16:53:05 +0200

    fix transmission config

--- services/transmission.nix
+++ services/transmission.nix
@@ -4,13 +4,17 @@
   services.transmission = {
     enable = true;
     settings = {
+      /* directory settings */
       "watch-dir" = "/srv/storage/Transmission/To-Download";
       "watch-dir-enabled" = true;
       "download-dir" = "/srv/storage/Transmission/Completed-Downloads";
       "incomplete-dir" = "/srv/storage/Transmission/Incomplete-Downloads";
       "incomplete-dir-enabled" = true;
+
       "rpc-enabled" = true;
       "rpc-whitelist" = "127.0.0.1,192.168.*.*";
+      "rpc-host-whitelist" = "void,192.168.*.*";
+      "rpc-host-whitelist-enabled" = true;
       "lpd-enabled" = true;
     };
   };
