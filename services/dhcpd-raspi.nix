bcae0749221b7bae7087dc0eff4779cc6e0cfb8e
Mogria <m0gr14@gmail.com>
2019-09-09 21:27:10 +0200

    dhcpd-raspi.nix: extract external and internal interface options

--- services/dhcpd-raspi.nix
+++ services/dhcpd-raspi.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, lib, ... }:
+let
+  externalInterface = "wlp2s0u2"; # wlp3s0
+  internalInterfaces = [ "enp3s0" ]; # enp4s0f1
+in {
   services.dhcpd4 = {
     enable = true;
-    interfaces = [ "enp4s0f1" ];
+    interfaces = internalInterfaces;
     extraConfig = ''
       option subnet-mask 255.0.0.0;
       option broadcast-address 10.255.255.255;
@@ -16,24 +19,26 @@
     machines = [
       {
         ethernetAddress = "a8:1e:84:d6:39:e7";
-        hostName = "raspberrypi.mshome.net";
+        hostName = "voidpi.nonet.test";
         ipAddress = "10.1.1.20";
       }
-      {
+      /* {
         ethernetAddress = "e0:3f:49:7f:b0:ab";
         hostName = "void";
         ipAddress = "10.1.1.23";
-      }
+      } */
     ];
   };
 
-  networking.interfaces.enp4s0f1.ipv4.addresses = [ { address = "10.1.1.35"; prefixLength = 24; } ];
-  networking.dhcpcd.denyInterfaces = [ "wlp3s0" ];
-  networking.networkmanager.unmanaged = [ "enp4s0f1" ];
+  networking.interfaces = lib.genAttrs internalInterfaces (interface: {
+    ipv4.addresses = [ { address = "10.1.1.35"; prefixLength = 8; } ];
+  });
+  networking.dhcpcd.denyInterfaces = [ externalInterface ];
+  networking.networkmanager.unmanaged = internalInterfaces;
 
   networking.nat = {
     enable = true;
-    externalInterface = "wlp3s0";
-    internalInterfaces = [ "enp4s0f1" ];
+    inherit externalInterface;
+    inherit internalInterfaces;
   };
 }
