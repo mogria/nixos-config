7a1cb65b4188b7a3472dce7d866645870fb7ee16
Mogria <m0gr14@gmail.com>
2019-09-09 21:44:15 +0200

    fix samba mount

--- services/samba-mount.nix
+++ services/samba-mount.nix
@@ -6,7 +6,7 @@
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in ["${automount_opts},credentials=/root/smb-secrets"];
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=30,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in ["${automount_opts},credentials=/.smb-secrets"];
   };
 }
