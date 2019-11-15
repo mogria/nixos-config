368c40c2b5c06b6db115b71ce1a8f104bdb2ba00
Mogria <m0gr14@gmail.com>
2019-11-15 22:07:43 +0100

    add forgotten changes to update rtl8821au

--- nix/overlays/packages.nix
+++ nix/overlays/packages.nix
@@ -2,6 +2,7 @@ self: super:
 
 let
   baseLinuxPackages = super.linuxPackages;
+  pkgs = self.pkgs;
 in {
 
   # Inspired by https://stackoverflow.com/questions/29075927/how-to-modify-a-nixos-package-configuration
@@ -13,6 +14,12 @@ in {
 
     rtl8821au = super.rtl8821au.overrideAttrs (attrs: {
       name = "rtl8821au-overridden";
+      src = pkgs.fetchFromGitHub {
+        owner = "abperiasamy";
+        repo = "rtl8812AU_8821AU_linux";
+        rev = "fa68771376a637c0b5f9cfa53da008570939a259";
+        sha256 = "0vm1zdssd6crfq7v1i0wjlbmr6fax1lg6d09jl2g85d2hyjw942f";
+      };
       meta = {
         platforms = [ "aarch64-linux" "x86_64-linux" ];
       };
@@ -33,7 +40,7 @@ in {
       tar -xzvf $downloadedFile --strip-components=1
       mkdir -p $out/share/consolefonts
       cp -R psf/*.psf $out/share/consolefonts
-      ${super.pkgs.gzip}/bin/gzip -n $out/share/consolefonts/*
+      ${pkgs.gzip}/bin/gzip -n $out/share/consolefonts/*
     '';
   };
 
