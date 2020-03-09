b198b7459181a221031b5edaa4044169c6f4a2bb
Mogria <m0gr14@gmail.com>
2020-03-09 20:35:06 +0100

    nvidiabl: add patch for GeForce MX150 to linuxPackages_latest as well
    So this way I can control the backlight on my laptop.

--- nix/overlays/packages.nix
+++ nix/overlays/packages.nix
@@ -1,30 +1,31 @@
 self: super:
 
 let
-  baseLinuxPackages = super.linuxPackages;
   pkgs = self.pkgs;
+  overlayLinuxPackages = basePackages:
+    basePackages.extend (self: super: {
+      nvidiabl = super.nvidiabl.overrideAttrs (attrs: {
+        # $ git diff nvidiabl-gpu.h | sed 's/overlays\///g' > nvidiabl-gpu.patch
+        patches = (if attrs ? patches then attrs.patches else [] )  ++ [ ./nvidiabl-gpu.patch ];
+      });
+    });
 in {
-
   # Inspired by https://stackoverflow.com/questions/29075927/how-to-modify-a-nixos-package-configuration
-  linuxPackages = baseLinuxPackages.extend (self: super: {
-    nvidiabl = super.nvidiabl.overrideAttrs (attrs: {
-      # $ git diff nvidiabl-gpu.h | sed 's/overlays\///g' > nvidiabl-gpu.patch
-      patches = (if attrs ? patches then attrs.patches else [] )  ++ [ ./nvidiabl-gpu.patch ];
-    });
-
-    rtl8821au = super.rtl8821au.overrideAttrs (attrs: {
-      name = "rtl8821au-overridden";
-      src = pkgs.fetchFromGitHub {
-        owner = "abperiasamy";
-        repo = "rtl8812AU_8821AU_linux";
-        rev = "fa68771376a637c0b5f9cfa53da008570939a259";
-        sha256 = "0vm1zdssd6crfq7v1i0wjlbmr6fax1lg6d09jl2g85d2hyjw942f";
-      };
-      meta = {
-        platforms = [ "aarch64-linux" "x86_64-linux" ];
-      };
-    });
+  linuxPackages = (overlayLinuxPackages super.linuxPackages).extend (self: super: {
+      rtl8821au = super.rtl8821au.overrideAttrs (attrs: {
+        name = "rtl8821au-overridden";
+        src = pkgs.fetchFromGitHub {
+          owner = "abperiasamy";
+          repo = "rtl8812AU_8821AU_linux";
+          rev = "fa68771376a637c0b5f9cfa53da008570939a259";
+          sha256 = "0vm1zdssd6crfq7v1i0wjlbmr6fax1lg6d09jl2g85d2hyjw942f";
+        };
+        meta = {
+          platforms = [ "aarch64-linux" "x86_64-linux" ];
+        };
+      });
   });
+  linuxPackages_latest = overlayLinuxPackages super.linuxPackages_latest;
 
   tamzen-console-font =  let version = "1.11.4"; in super.pkgs.fetchFromGitHub {
     name = "tamzen-console-font-${version}";
