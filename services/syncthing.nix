1aaf27ce8488ddcb2bffb1c8fd6e370c22feea1b
Mogria <m0gr14@gmail.com>
2020-11-11 01:47:10 +0100

    syncthing: add id for void

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -13,7 +13,7 @@
 
     declarative = {
       devices = {
-        void = { id = "void"; addresses = [ "tcp://192.168.1.2" "tcp://void.home" ]; introducer = true; };
+        void = { id = "YBWFQ4K-TYDME37-FCMJPBV-QSPIQ45-GL7WJT4-SBO4NHN-CE5HXUH-3HGQKAW"; addresses = [ "tcp://192.168.1.2" "tcp://void.home" ]; introducer = true; };
         voidbook = { id = "X7M3IX6-J47VWTQ-AAH5CTQ-SSZYVUE-RZJPNF7-JDJZ4PX-QMKPCXC-HX23LQC"; addresses = [ "tcp://voidbook.home" ]; introducer = true; };
       };
 
