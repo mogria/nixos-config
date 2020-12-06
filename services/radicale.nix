144ee1b3ec530771d89e6d07d0fc42a152467025
Mogria <m0gr14@gmail.com>
2020-12-06 15:50:42 +0100

    radicle: merge config to use default port setting

--- services/radicale.nix
+++ services/radicale.nix
@@ -1,13 +1,16 @@
 { pkgs, config, ... }:
 
-{
+let
+  port = 5232;
+in {
+    networking.firewall.allowedTCPPorts = [ port ];
 	services.radicale = {
 	    enable = true;
 	    config =
 		''
 		[server]
-		# Bind all interfaces on port 5232 
-		hosts = 0.0.0.0:5232
+		# Bind all interfaces on port ${builtins.toString port}
+		hosts = 0.0.0.0:${builtins.toString port}
 		'';
 	};
 }
