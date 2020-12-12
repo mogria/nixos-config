f90612d5fe09c709746ee3f6bc818f07e0e8ebb3
Mogria <m0gr14@gmail.com>
2020-12-12 15:47:48 +0100

    gitolite: add usage notes

--- services/gitolite.nix
+++ services/gitolite.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  # Use this git command to add a remote repo
+  # $ git remote add origin  gitolite@void.home:$repo_name.git 
+  #
+  # The gitolite-admin repo contains the server configuration & a push will apply it.
   services.gitolite = {
     enable = true;
     adminPubkey = builtins.readFile ../users/keys/mogria_void_ecdsa.pub;
