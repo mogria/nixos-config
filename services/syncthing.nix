ee28260ac5fcb538e29920c92fd5b99ceab364a1
Mogria <m0gr14@gmail.com>
2021-01-11 23:21:29 +0100

    syncthing: add more folders, configure paths for different machines

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -1,40 +1,57 @@
 { pkgs, config, ... }:
 
-{
+let
+    dataDir = "/home/syncthing/data";
+    devices = {
+      void = { id = "YBWFQ4K-TYDME37-FCMJPBV-QSPIQ45-GL7WJT4-SBO4NHN-CE5HXUH-3HGQKAW"; addresses = [ "tcp://192.168.1.2" "tcp://void.home" ]; introducer = true; };
+      voidbook = { id = "X7M3IX6-J47VWTQ-AAH5CTQ-SSZYVUE-RZJPNF7-JDJZ4PX-QMKPCXC-HX23LQC"; addresses = [ "tcp://voidbook.home" ]; };
+      voidphone = { id = "ALXKNUB-MFR2SXS-X56V2PD-572J3NO-K5U5UQN-BP4IFFJ-PYCHZIQ-ZIJGMQ4"; addresses = [ "tcp://voidphone" ]; };
+    };
+    deviceNames = builtins.attrNames devices;
+
+in {
   services.syncthing = {
     enable = true;
     configDir = "/home/syncthing/.config/syncthing";
-    dataDir = "/home/syncthing/data";
-
-    # package
-
+    inherit dataDir;
 
     # all_proxy
-
-    group = "syncthing";
+    user = "syncthing";
+    group = "users";
 
     declarative = {
-      devices = {
-        void = { id = "YBWFQ4K-TYDME37-FCMJPBV-QSPIQ45-GL7WJT4-SBO4NHN-CE5HXUH-3HGQKAW"; addresses = [ "tcp://192.168.1.2" "tcp://void.home" ]; introducer = true; };
-        voidbook = { id = "X7M3IX6-J47VWTQ-AAH5CTQ-SSZYVUE-RZJPNF7-JDJZ4PX-QMKPCXC-HX23LQC"; addresses = [ "tcp://voidbook.home" ]; introducer = true; };
-        voidphone = { id = "ALXKNUB-MFR2SXS-X56V2PD-572J3NO-K5U5UQN-BP4IFFJ-PYCHZIQ-ZIJGMQ4"; addresses = [ "tcp://voidphone" ]; };
-      };
+      inherit devices;
 
       folders = {
-        "/home/syncthing/data/share" = {
+        "${dataDir}/share" = {
           label = "Public Share";
           id = "share";
-          devices = [ "void" "voidbook" "voidphone" ];
+          devices = deviceNames;
         };
-        "/home/syncthing/data/mogria-private" = {
+        "${dataDir}/mogria-private" = {
           label = "mogria-private";
           id = "3a6tf-3jkdn";
-          devices = [ "void" "voidbook" "voidphone" ];
+          devices = deviceNames;
         };
-        "/home/syncthing/data/Android Camera" = {
+        "/home/mogria/Music" = {
+          label = "Music";
+          id = "pckpd-j4nad";
+          devices = deviceNames;
+        };
+        "${dataDir}/Android Camera" = {
           label = "Android Camera";
           id = "lg-h870_uduj-photos";
-          devices = [ "void" "voidbook" "voidphone" ];
+          devices = deviceNames;
+        };
+        "${dataDir}/Syncthing Camera" = {
+          label = "Syncthing Camera";
+          id = "syncthingAndroidCamera-52x89-60es4";
+          devices = deviceNames;
+        };
+        "${dataDir}/old-dropbox" = {
+          label = "Android Camera";
+          id = "lg-h870_uduj-photos";
+          devices = deviceNames;
         };
       };
 
@@ -49,5 +66,5 @@
   # network.firewall.allowedTCPPorts = [ 22000 ];
 
   # so that all desktop users can access syncthing
-  users.users.syncthing.extraGroups = [ "users" ];
+  users.users.syncthing.extraGroups = [ "syncthing" "users" ];
 }
