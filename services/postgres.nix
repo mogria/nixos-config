f4735a19da502b233166016131c2a8e8f85af453
Mogria <m0gr14@gmail.com>
2021-01-19 15:28:02 +0100

    postgres: expand config, use postgres 13

--- services/postgres.nix
+++ services/postgres.nix
@@ -1,7 +1,19 @@
 { config, pkgs, ... }:
 
-{
+let
+  postgresPackage = pkgs.postgresql_13;
+in {
   services.postgresql = {
     enable = true;
+    enableTCPIP = true;
+    package = postgresPackage;
+
+    # authentication = ''
+    # '';
+    #
+    # settings = {
+    # };
+
+    extraPlugins = with postgresPackage.pkgs; [ tsearch_extras pg_similarity ];
   };
 }
