df769362df44169d6f04af1645ae58a6e9b4e567
Mogria <m0gr14@gmail.com>
2021-10-03 18:32:35 +0200

    opengl: enable 32bit libraries for gaming

--- hardware/opengl.nix
+++ hardware/opengl.nix
@@ -5,8 +5,8 @@
     enable = true;
     extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau intel-ocl ];
 
-    # Disable 32bit stuff, as I don't plan to run games/wine/old graphics stuff...
-    # driSupport32Bit = true;
-    # extraPackages32 = with pkgs.driversi686Linux; [ glxinfo vaapiIntel libvdpau-va-gl vaapiVdpau ];
+    # Enable 32bit stuff for games in steam
+    driSupport32Bit = true;
+    extraPackages32 = with pkgs.driversi686Linux; [ glxinfo vaapiIntel libvdpau-va-gl vaapiVdpau ];
   };
 }
