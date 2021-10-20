3682cc9553556aa3a1b406ceafdb7decf078225e
Mogria <m0gr14@gmail.com>
2021-10-20 22:30:46 +0200

    xkbd: ALT+SHIFT to switch keyboard layout, menu key is compose key

--- graphical/desktop.nix
+++ graphical/desktop.nix
@@ -14,7 +14,9 @@
   services.xserver.enable = true;
   services.xserver.layout = "us";
   services.xserver.xkbModel = "pc104";
-  services.xserver.xkbOptions = "terminate:ctrl_alt_bksp, eurosign:e";
+  # Switch Keyboard Layout with ALT+SHIFT and light up scroll lock when switching
+  # use menu key as compose key
+  services.xserver.xkbOptions = "terminate:ctrl_alt_bksp,eurosign:e,compose:menu,grp:alt_shift_toggle,grp_led:scroll";
 
   # Enable networking via network manager,
   # this enables GUI configuration of network connections/WLANs
