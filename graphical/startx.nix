31566ad30555d494df5a1523b0f41410744e3775
Mogria <m0gr14@gmail.com>
2021-10-29 15:37:30 +0200

    xorg: try out stuff using just startx

--- graphical/startx.nix
+++ graphical/startx.nix
@@ -1,8 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  services.xserver.enable = true;
   # Use this when you don't want to use a display manager
   # like lightdm and start the X server using startx
   # in one of your TTYs
   services.xserver.displayManager.startx.enable = true;
+
+  # environment.systemPackages = with pkgs; [ sx ];
+  #environment.systemPackages = with pkgs; [ xorg.xinit ];
 }
