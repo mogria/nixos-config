4cd83d4c4bcc4ca548fd71070fb8c2f6845ef272
Mogria <m0gr14@gmail.com>
2021-10-29 15:44:37 +0200

    try out stuff with rasperry dhcpd configuration

--- services/dhcpd-raspi.nix
+++ services/dhcpd-raspi.nix
@@ -1,15 +1,16 @@
 { config, pkgs, lib, ... }:
 
 let
-  externalInterface = "eth0"; # wlp3s0
-  internalInterfaces = [ "wlan0" ]; # enp4s0f1
+  externalInterface = "enp4s0r1"; # "wlp3s0"; # "eth0"; # wlp3s0
+  internalInterfaces = [ "wlp3s0" ]; # [ "wlan0" ]; # enp4s0f1
   # note: setting up the same net for multiple internal
   # interfaces doesn't work (yet),
   # only the first one gets initialized
   netNo = "23";
+  domain = "mynet.test";
 in {
   networking.hosts = {
-    "192.168.${netNo}.1" = [ "noroute" "noroute.nonet.test" ];
+    # "192.168.${netNo}.1" = [ "raspi4" "raspi4.mynet.test" ];
   };
 
   services.dnsmasq = {
@@ -19,7 +20,7 @@ in {
     extraConfig = ''
       ${builtins.concatStringsSep "\n" (map (interface: "interface=${interface}") internalInterfaces)}
 
-      domain=nonet.test
+      domain=mynet.test
     '';
   };
   services.dhcpd4 = {
@@ -30,17 +31,17 @@ in {
       option broadcast-address 192.168.${netNo}.255;
       option routers 192.168.${netNo}.1;
       option domain-name-servers 192.168.${netNo}.1, 1.1.1.1;
-      option domain-name "nonet.test";
+      option domain-name "mynet.test";
       subnet 192.168.${netNo}.0 netmask 255.255.255.0 {
         range 192.168.${netNo}.20 192.168.${netNo}.99;
       }
     '';
     machines = [
-      {
-        ethernetAddress = "e0:3f:49:7f:b0:ab";
-        hostName = "void";
-        ipAddress = "192.168.${netNo}.2";
-      }
+     {
+       ethernetAddress = "dc:a6:32:21:2e:69";
+       hostName = "raspi4";
+       ipAddress = "192.168.${netNo}.2";
+     }
     ];
   };
 
